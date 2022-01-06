c3248d3b8f637116115263c98231abefc84116cc
Josh Wong <joshwong74@gmail.com>
2022-01-06 14:19:28 -0800

    Updated example ComponentName.tsx

---
+++ frontend/src/components/ComponentName/ComponentName.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+export default function ComponentName() {
+  return (
+    <div>
+        <h1>Hello World!</h1>
+        <p>This is an example screen</p>
+    </div>
+  );
+}
