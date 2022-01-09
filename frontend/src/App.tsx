e8883dbacfe120427c0f6e354a7397b5ef2055f9
Tessa Tapmongkol <ttapmong@calpoly.edu>
2022-01-08 16:57:01 -0800

    Added donator navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 
 /* Screens */
 // import ComponentName from './components/ComponentName/ComponentName';
-import LoginPage from "./components/LoginPage/LoginPage";
+// import LoginPage from "./components/LoginPage/LoginPage";
+import DonatorNavbar from './components/DonatorNavbar/DonatorNavbar';
 
 function App(): JSX.Element {
   return (
-    <LoginPage />
+    <body>
+      <DonatorNavbar />
+    </body>
   );
 }
 
