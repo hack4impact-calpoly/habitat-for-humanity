dad3239b8588f27e95302827041e611a92eebbbb
scallait <sebastiencallait@gmail.com>
2022-01-10 21:35:41 -0800

    Added responsive screen handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 /* Screens */
 // import ComponentName from './components/ComponentName/ComponentName';
 import LoginPage from "./components/LoginPage/LoginPage";
+import CreateAccountPage from "./components/CreateAccountPage/CreateAccountPage";
 
 function App(): JSX.Element {
   return (
