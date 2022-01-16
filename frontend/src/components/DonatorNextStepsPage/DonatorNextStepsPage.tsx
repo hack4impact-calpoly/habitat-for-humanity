ee5665e06544d5574d1632b158dad682f318bb40
Tessa Tapmongkol <ttapmong@calpoly.edu>
2022-01-16 11:45:29 -0800

    Added clickable phone number

--- frontend/src/components/DonatorNextStepsPage/DonatorNextStepsPage.tsx
+++ frontend/src/components/DonatorNextStepsPage/DonatorNextStepsPage.tsx
@@ -10,7 +10,7 @@ const DonatorNextStepsPage = (): JSX.Element => {
                 <h2 id="donNextStepsHeader">Confirmation and Next Steps</h2>
                 <p id="donNextStepsDesc">You have successfully submitted your donation form! Our staff will review your donation in the next few days. Please expect an email from us with updates regarding your donation pick up or drop off.</p>
                 <p id="donNextStepsQuestions">If you have any questions or concerns, please contact us at </p>
-                <p id="donNextStepsPhone">(805) 546-8699</p>
+                <a style={{ textDecoration: "none" }} href="tel: +8055468699"><p id="donNextStepsPhone">(805) 546-8699</p></a>
             </div>
         </body>
     )
