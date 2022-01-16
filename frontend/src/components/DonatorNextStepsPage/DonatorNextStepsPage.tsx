7ec4f93946bbbf542c2d3554e5e4e315a8231000
Tessa Tapmongkol <ttapmong@calpoly.edu>
2022-01-15 23:33:37 -0800

    Added boilerplate, need to fix styling

---
+++ frontend/src/components/DonatorNextStepsPage/DonatorNextStepsPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import DonatorNavbar from '../DonatorNavbar/DonatorNavbar';
+require("./DonatorNextStepsPage.css");
+
+const DonatorNextStepsPage = (): JSX.Element => {
+    return (
+        <body>
+            <DonatorNavbar />
+            <div id="donNextStepsPage">
+                <h2 id="donNextStepsHeader">Confirmation and Next Steps</h2>
+                <p id="donNextStepsDesc">You have successfully submitted your donation form! Our staff will review your donation in the next few days. Please expect an email from us with updates regarding your donation pick up or drop off.</p>
+                <p id="donNextStepsQuestions">If you have any questions or concerns, please contact us at </p>
+                <p id="donNextStepsPhone">(805) 546-8699</p>
+            </div>
+        </body>
+    )
+}
+
+export default DonatorNextStepsPage;
