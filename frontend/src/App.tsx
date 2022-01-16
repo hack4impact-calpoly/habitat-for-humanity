ce2d98d6c72946f20c375041ddb246d27a6f557f
Tessa Tapmongkol <ttapmong@calpoly.edu>
2022-01-15 23:33:37 -0800

    Added boilerplate, need to fix styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
 import './App.css';
 import React from 'react'
+import DonatorNextStepsPage from './components/DonatorNextStepsPage/DonatorNextStepsPage';
 
 /* Screens */
 // import ComponentName from './components/ComponentName/ComponentName';
-import LoginPage from "./components/LoginPage/LoginPage";
+// import LoginPage from "./components/LoginPage/LoginPage";
 // import DonatorNavbar from './components/DonatorNavbar/DonatorNavbar';
-import DonatorHomePage from './components/DonatorHomePage/DonatorHomePage';
-import CreateAccountPage from './components/CreateAccountPage/CreateAccountPage';
-import DonatorProfileEditPage from './components/DonatorProfileEditPage/DonatorProfileEditPage';
+// import DonatorHomePage from './components/DonatorHomePage/DonatorHomePage';
+// import CreateAccountPage from './components/CreateAccountPage/CreateAccountPage';
+// import DonatorProfileEditPage from './components/DonatorProfileEditPage/DonatorProfileEditPage';
 
 function App(): JSX.Element {
   return (
     //<LoginPage />
     //<DonatorHomePage />
     //<CreateAccountPage />
-    <DonatorProfileEditPage />
+    // <DonatorProfileEditPage />
+    <DonatorNextStepsPage />
   );
 }
 
