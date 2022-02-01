61ec71b63a4aabcbd40bff0788d62f1e4a98d5f7
Sebastien Callait <71291167+scallait@users.noreply.github.com>
2022-02-01 14:32:18 -0800

    Added links to DonatorHomePage

--- frontend/src/components/DonatorHomePage/DonatorHomePage.tsx
+++ frontend/src/components/DonatorHomePage/DonatorHomePage.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import DonatorNavbar from "../DonatorNavbar/DonatorNavbar";
 import gift from "./../../images/gift.png";
 require("./DonatorHomePage.css")
 
 const DonatorHomePage = (): JSX.Element => {
+    let navigate = useNavigate();
+
+    const routeChange = () : void => {
+        let path = 'donate/location';
+        navigate(path);
+    }
+
     return (
         <body id="donatePage">
             <DonatorNavbar />
@@ -11,7 +19,7 @@ const DonatorHomePage = (): JSX.Element => {
                 <img id="donatePageGift" src={gift} alt="gift" />
                 <h1 id="donatePageHeader">Welcome, make a donation!</h1>
                 <p id="donatePageDesc">To schedule a pickup or drop off your donation, click below to enter information about the item for our staff to review.</p>
-                <button id="donatePageButton">Make a Donation</button>
+                <button id="donatePageButton" onClick={routeChange}>Make a Donation</button>
             </div>
         </body>
     )
