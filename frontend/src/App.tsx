a2c640cbefe851de81b47e246aa9e34a0db48a15
Sebastien Callait <71291167+scallait@users.noreply.github.com>
2022-02-01 18:53:32 -0800

    Can navigate to all pages from login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,25 +3,25 @@ import React from 'react'
 
 /* Screens */
 // import ComponentName from './components/ComponentName/ComponentName';
-// import LoginPage from "./components/LoginPage/LoginPage";
+import LoginPage from "./components/LoginPage/LoginPage";
 // import DonatorNavbar from './components/DonatorNavbar/DonatorNavbar';
 // import DonatorProfilePage from './components/DonatorProfile/DonatorProfile';
 // import DonatorHomePage from './components/DonatorHomePage/DonatorHomePage';
 // import CreateAccountPage from './components/CreateAccountPage/CreateAccountPage';
 // import DonatorProfileEditPage from './components/DonatorProfileEditPage/DonatorProfileEditPage';
 // import DonatorLocationPage from './components/DonationLocationPage/DonatorLocationPage';
-import DonatorNextStepsPage from './components/DonatorNextStepsPage/DonatorNextStepsPage';
+// import DonatorNextStepsPage from './components/DonatorNextStepsPage/DonatorNextStepsPage';
 
 
 function App(): JSX.Element {
   return (
-    //<LoginPage />
+    <LoginPage />
     //<DonatorHomePage />
     //<CreateAccountPage />
     // <DonatorProfileEditPage />
     // <DonatorLocationPage />
     // <LoginPage />
-    <DonatorNextStepsPage />
+    //<DonatorNextStepsPage />
   );
 }
 
