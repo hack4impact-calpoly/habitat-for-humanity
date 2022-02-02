d9ab4f556cd92b0abaf5cbb6b30667a761d43ceb
Sebastien Callait <71291167+scallait@users.noreply.github.com>
2022-02-01 18:39:35 -0800

    Added links to donator next steps page

--- frontend/src/components/DonatorNextStepsPage/DonatorNextStepsPage.tsx
+++ frontend/src/components/DonatorNextStepsPage/DonatorNextStepsPage.tsx
@@ -7,14 +7,10 @@ const DonatorNextStepsPage = (): JSX.Element => {
     let navigate = useNavigate();
 
     const buttonNavigation = (e : React.MouseEvent<HTMLButtonElement>) : void => {
-        const backPath : string = ""; //TBD
-        const nextPath : string = "/donator";
+        const donePath : string = "/donator";
         
-        if(e.currentTarget.value === "backButton"){
-            navigate(backPath);
-        }
-        else if(e.currentTarget.value === "nextButton"){
-            navigate(nextPath);
+        if(e.currentTarget.value === "doneButton"){
+            navigate(donePath);
         }
     }
 
@@ -26,9 +22,7 @@ const DonatorNextStepsPage = (): JSX.Element => {
                 <p id="donNextStepsDesc">You have successfully submitted your donation form! Our staff will review your donation in the next few days. Please expect an email from us with updates regarding your donation pick up or drop off.</p>
                 <p id="donNextStepsQuestions">If you have any questions or concerns, please contact us at </p>
                 <a style={{ textDecoration: "none" }} href="tel: +8055468699"><p id="donNextStepsPhone">(805) 546-8699</p></a>
-                {// TODO: Add links to next and back page
-                }
-                <button className='doneButton'>Done</button>
+                <button value="doneButton" className='doneButton' onClick={buttonNavigation}>Done</button>
             </div>
         </body>
     )
