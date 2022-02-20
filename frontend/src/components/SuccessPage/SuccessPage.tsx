fc256641707506247acbe23b569f52a49d8198e2
Tessa Tapmongkol <ttapmong@calpoly.edu>
2022-02-20 13:03:10 -0800

    72-Success-Page (resolves #72)

---
+++ frontend/src/components/SuccessPage/SuccessPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import checkCircle from '../../images/CheckCircle.png';
+require("./SuccessPage.css")
+
+const SuccessPage = (): JSX.Element => {
+    return (
+        <>
+            <div id="successBox">
+                <img id="checkCircle" src={checkCircle} alt="checkCircle" />
+                <h1>Success</h1>
+                <p>You have successfully signed up for an account.</p>
+                <button id="successButton">Log In</button>
+            </div>
+        </>
+    )
+}
+
+export default SuccessPage;
