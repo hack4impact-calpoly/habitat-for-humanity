d557ce636734d90b1e308cc95b524c5dd05f48b7
Josh Wong <joshwong74@gmail.com>
2022-02-23 17:19:27 -0800

    Resorted files to authentication and donator

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,23 @@ import Amplify from '@aws-amplify/core';
 import { Auth } from '@aws-amplify/auth';
 // import awsconfig from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-import LoginPage from "./components/LoginPage/LoginPage";
+import LoginPage from "./components/authentication/LoginPage/LoginPage";
 
 /* Screens */
-// import ComponentName from './components/ComponentName/ComponentName';
-// import DonatorNavbar from './components/DonatorNavbar/DonatorNavbar';
-// import DonatorProfilePage from './components/DonatorProfile/DonatorProfile';
-// import DonatorHomePage from './components/DonatorHomePage/DonatorHomePage';
+// import DonatorNavbar from './components/donator/DonatorNavbar/DonatorNavbar';
+// import DonatorProfilePage from './components/donator/DonatorProfile/DonatorProfile';
+// import DonatorHomePage from './components/donator/DonatorHomePage/DonatorHomePage';
 // import CreateAccountPage from './components/CreateAccountPage/CreateAccountPage';
-// import DonatorProfileEditPage from './components/DonatorProfileEditPage/DonatorProfileEditPage';
-import DonatorLocationPage from './components/DonationLocationPage/DonatorLocationPage';
-//import DonatorNextStepsPage from './components/DonatorNextStepsPage/DonatorNextStepsPage';
+// import DonatorProfileEditPage from './components/donator/DonatorProfileEditPage/DonatorProfileEditPage';
+import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
+//import DonatorNextStepsPage from './components/donator/DonatorNextStepsPage/DonatorNextStepsPage';
 
-import SubmitDropOffPage from './components/donation/SubmitDropOffPage/SubmitDropOffPage';
-import SubmitPickUpPage from './components/donation/SubmitPickUpPage/SubmitPickUpPage';
-import SubmitPickUpMultiplePhotoPage from './components/donation/SubmitPickUpMultiplePhotoPage/SubmitPickUpMultiplePhotoPage';
-// import DonatorLocationPage from './components/DonationLocationPage/DonatorLocationPage';
-// import DonatorNextStepsPage from './components/DonatorNextStepsPage/DonatorNextStepsPage';
-// import Donation from './components/Donation/Donation';
+import SubmitDropOffPage from './components/donator/donation/SubmitDropOffPage/SubmitDropOffPage';
+import SubmitPickUpPage from './components/donator/donation/SubmitPickUpPage/SubmitPickUpPage';
+import SubmitPickUpMultiplePhotoPage from './components/donator/donation/SubmitPickUpMultiplePhotoPage/SubmitPickUpMultiplePhotoPage';
+// import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
+// import DonatorNextStepsPage from './components/donator/DonatorNextStepsPage/DonatorNextStepsPage';
+// import Donation from './components/donator/Donation/Donation';
 // Amplify.configure(awsconfig);
 
 
---
+++ frontend/src/components/authentication/LoginPage/LoginPage.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import VisibilityIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOffOutlined';
+import InputAdornment from '@mui/material/InputAdornment';
+import Input from '@mui/material/Input';
+import IconButton from '@mui/material/IconButton';
+
+import logo from "images/logo.png";
+import ForgotPasswordPage from "../ForgotPasswordPage/ForgotPasswordPage";
+require("./LoginPage.css");
+
+const LoginPage = (): JSX.Element => {
+    const [email, setEmail] = React.useState<string>("");
+    const [password, setPassword] = React.useState({
+                                                    value: "",
+                                                    showPassword: false});
+    let navigate = useNavigate();
+
+    const forgotPasswordPath = "/ForgotPassword";
+    const createAccountPath = "/CreateAccount";
+
+    const login = () : void => {
+        let valid = checkCredentials();
+        if (valid /*&& donator*/){
+            navigate("/Donor");
+        }
+        /*
+        else if (valid && admin){
+            navigate("/Admin/Home");
+        }
+        else if (valid && volunteer){
+            navigate("/Donator/Home");
+        }
+        else{
+            alert("Sorry an unexpected error occured while logging you in");
+        }
+        */
+    }
+    const checkCredentials = () : boolean => {
+        if (email === "") {
+            alert("Email is blank. Please try again.")
+            return false;
+        } else if (password.value === "") {
+            alert("Password is blank. Please try again.");
+            return false;
+        } // check other invalid errors
+        else{
+            return true;
+        }
+        // if no errors/valid login -> redirect to logged in page
+    }
+
+    return (
+        <body>
+            <div id="loginBox">
+                <img src={logo} alt="logo" id="loginLogo" />
+                <form id="loginForm">
+                    <p className="loginLabel">Email</p>
+                    <input
+                        className="loginInput"
+                        type="text"
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event?.target?.value)}
+                    />
+                    <div id="loginPassword">
+                        <p className="loginLabel">Password</p>
+                        <Link to={forgotPasswordPath} id="loginForgotPassword">Forgot Password?</Link>
+                    </div>
+                    <Input
+                        className="loginInput"
+                        id="passwordBox"
+                        value={password.value}
+                        type={password.showPassword ? "text" : "password"}
+                        disableUnderline={true}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword({...password, "value": event?.target?.value})}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    onClick={() => setPassword({...password, showPassword: !password.showPassword})}
+                                    onMouseDown={(event: React.MouseEvent<HTMLButtonElement>) => event?.preventDefault()}
+                                    edge="end">
+                                        {password.showPassword ? <VisibilityIcon className="passwordIcon"/> : <VisibilityOffIcon className="passwordIcon" />}
+                                    </IconButton>
+                            </InputAdornment>
+                        }
+                    />
+                    <button id="loginSubmit" onClick={login}>Log In</button>
+                </form>
+                <div style={{ textAlign: "right", marginTop: "10px" }}>
+                    <p className="loginCreateAccount">Don't have an account? </p>
+                    <Link to={createAccountPath} className="loginCreateAccount" id="createAccountLink">Create Account</Link>
+                </div>
+            </div>
+        </body>
+    );
+};
+
+export default LoginPage;
