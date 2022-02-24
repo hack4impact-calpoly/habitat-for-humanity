f8282055052811dc4260104fc9bd97e198803421
Brandon Huynh <brandon.huynh@live.com>
2022-02-23 19:35:57 -0800

    Added more basic GET requests

--- backend/routes/eventRoutes.ts
+++ backend/routes/eventRoutes.ts
@@ -23,6 +23,36 @@ router.get("/:eventId", async (req: Request, res: Response) => {
   }
 })
 
+//get all events by title
+router.get("/:title", async (req: Request, res: Response) => {
+  try {
+    const events = await Event.find({title: req.params.title})
+    res.send(events)
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+//get all events by start_date
+router.get("/:startDate", async (req: Request, res: Response) => {
+  try {
+    const events = await Event.find({start_date: req.params.startDate})
+    res.send(events)
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
+//get all events by end_date
+router.get("/:endDate", async (req: Request, res: Response) => {
+  try {
+    const events = await Event.find({end_date: req.params.endDate})
+    res.send(events)
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
+
 //get all events by location
 router.get("/:location", async (req: Request, res: Response) => {
   try {
@@ -32,5 +62,26 @@ router.get("/:location", async (req: Request, res: Response) => {
     res.status(400).send(error);
   }
 })
+
+router.use(express.json());
+
+//posts a new Event to EventsDB
+router.post('/', async (req: Request, res: Response) => {
+  const { title, start_date, end_date, location } = req.body;
+  let event = new Event({
+    title,
+    start_date,
+    end_date,
+    location
+  });
+
+  try {
+    event = await event.save();
+    console.log(event);
+    res.json(event);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 module.exports = router
