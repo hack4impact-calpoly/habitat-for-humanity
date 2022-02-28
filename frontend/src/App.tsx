f19bfcf1f934cf6554827ae27dda149167d98f22
megantrieu <meganatrieu@gmail.com>
2022-02-28 00:56:32 -0800

    responsive mobile version with multiple photos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,25 @@
 import './App.css';
 import React from 'react'
-// import Amplify from '@aws-amplify/core';
-// import { Auth } from '@aws-amplify/auth';
-// import awsconfig from './aws-exports';
-import { withAuthenticator } from '@aws-amplify/ui-react';
 
-/* Authentication Screens */
+/* Screens */
+// import ComponentName from './components/ComponentName/ComponentName';
+// import LoginPage from "./components/LoginPage/LoginPage";
+// import DonatorNavbar from './components/DonatorNavbar/DonatorNavbar';
+import DonatorProfilePage from './components/donator/DonatorProfile/DonatorProfile';
+// import DonatorHomePage from './components/DonatorHomePage/DonatorHomePage';
 // import CreateAccountPage from './components/CreateAccountPage/CreateAccountPage';
-import LoginPage from "./components/authentication/LoginPage/LoginPage";
-// import SuccessPage from './components/authentication/SuccessPage/SuccessPage';
+// import DonatorProfileEditPage from './components/DonatorProfileEditPage/DonatorProfileEditPage';
+import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
+//import DonatorNextStepsPage from './components/DonatorNextStepsPage/DonatorNextStepsPage';
 
-
-/* Donator Screens */
-// import DonatorNavbar from './components/donator/DonatorNavbar/DonatorNavbar';
-// import DonatorProfilePage from './components/donator/DonatorProfile/DonatorProfile';
-// import DonatorHomePage from './components/donator/DonatorHomePage/DonatorHomePage';
-// import DonatorProfileEditPage from './components/donator/DonatorProfileEditPage/DonatorProfileEditPage';
-// import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
-//import DonatorNextStepsPage from './components/donator/DonatorNextStepsPage/DonatorNextStepsPage';
-// import SubmitDropOffPage from './components/donator/donation/SubmitDropOffPage/SubmitDropOffPage';
-// import SubmitPickUpPage from './components/donator/donation/SubmitPickUpPage/SubmitPickUpPage';
-// import SubmitPickUpMultiplePhotoPage from './components/donator/donation/SubmitPickUpMultiplePhotoPage/SubmitPickUpMultiplePhotoPage';
-// import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
-// import DonatorNextStepsPage from './components/donator/DonatorNextStepsPage/DonatorNextStepsPage';
-// import Donation from './components/donator/Donation/Donation';
-
-
-// Amplify.configure(awsconfig);
+import SubmitDropOffPage from './components/donator/donation/SubmitDropOffPage/SubmitDropOffPage';
+import SubmitPickUpPage from './components/donator/donation/SubmitPickUpPage/SubmitPickUpPage';
+import SubmitPickUpMultiplePhotoPage from './components/donator/donation/SubmitPickUpMultiplePhotoPage/SubmitPickUpMultiplePhotoPage';
 
 
 function App(): JSX.Element {
   return (
-    // <SuccessPage />
-    <LoginPage />
+    //<LoginPage />
     //<DonatorHomePage />
     //<CreateAccountPage />
     // <DonatorProfileEditPage />
@@ -42,10 +29,8 @@ function App(): JSX.Element {
 
     //<SubmitDropOffPage />
     //<SubmitPickUpPage />
-    //<SubmitPickUpMultiplePhotoPage />
-    //<DonatorNextStepsPage />
+    <SubmitPickUpMultiplePhotoPage />
   );
 }
 
-// export default withAuthenticator(App);
 export default App;
