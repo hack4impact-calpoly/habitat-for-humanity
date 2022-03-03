6b066bd921ace6f847b52fc74758f230c92f6ef3
Brandon Huynh <brandon.huynh@live.com>
2022-03-02 17:15:46 -0800

    Added POST and PUT requests for User

--- backend/routes/userRoutes.ts
+++ backend/routes/userRoutes.ts
@@ -33,7 +33,7 @@ router.get("/volunteers", async (req: Request, res: Response) => {
   }
 })
 
-// //get all donor
+//get all donors
 router.get("/donors", async (req: Request, res: Response) => {
   try {  
     const donor = await User.find({userType: "Donor"})
@@ -50,6 +50,134 @@ router.get("/admins", async (req: Request, res: Response) => {
     res.send(admins)
   } catch (error) {
     res.status(400).send(error);
+  }
+})
+
+//add new User to UsersDB
+router.post("/", async (req: Request, res: Response) => {
+  try {  
+    const { 
+      userType,
+      firstName,
+      lastName,
+      email,
+      phone,
+    } = req.body;
+    const newUser = new User({
+      userType,
+      firstName,
+      lastName,
+      email,
+      phone,
+    });
+    // console.log(newUser);
+    await newUser.save();
+    res.send(`${firstName} ${lastName} added to the userDB`);
+  } catch (error) {
+    let errorMessage;
+    if (error instanceof Error) { 
+      errorMessage = error.message; 
+    } else { 
+      errorMessage = String(errorMessage); 
+    }
+    res.status(400).send(errorMessage);
+    console.log(`Error: ${errorMessage}`);
+  }
+})
+
+//update user firstName
+router.put("/:userId/firstName", async (req: Request, res: Response) => {
+  try {
+    let user = await User.findOne({ _id: req.params.userId })
+    user.firstName = req.body.firstName;
+
+    await user.save();
+
+    res.send(
+      `Updated user "${req.params.userId}" with new firstName: "${req.body.firstName}"`
+    );
+    
+  } catch (error) {
+    let errorMessage;
+    if (error instanceof Error) { 
+      errorMessage = error.message; 
+    } else { 
+      errorMessage = String(errorMessage); 
+    }
+    res.status(400).send(errorMessage);
+    console.log(`Error: ${errorMessage}`);
+  }
+})
+
+//update user lastName
+router.put("/:userId/lastName", async (req: Request, res: Response) => {
+  try {
+    let user = await User.findOne({ _id: req.params.userId })
+    user.lastName = req.body.lastName;
+
+    await user.save();
+
+    res.send(
+      `Updated user "${req.params.userId}" with new lastName: "${req.body.lastName}"`
+    );
+    
+  } catch (error) {
+    let errorMessage;
+    if (error instanceof Error) { 
+      errorMessage = error.message; 
+    } else { 
+      errorMessage = String(errorMessage); 
+    }
+    res.status(400).send(errorMessage);
+    console.log(`Error: ${errorMessage}`);
+  }
+})
+
+//update user email
+router.put("/:userId/email", async (req: Request, res: Response) => {
+  try {
+    let user = await User.findOne({ _id: req.params.userId })
+    user.email = req.body.email;
+
+    await user.save();
+
+    res.send(
+      `Updated user "${req.params.userId}" with new email: "${req.body.email}"`
+    );
+    
+  } catch (error) {
+    let errorMessage;
+    if (error instanceof Error) { 
+      errorMessage = error.message; 
+    } else { 
+      errorMessage = String(errorMessage); 
+    }
+    res.status(400).send(errorMessage);
+    console.log(`Error: ${errorMessage}`);
+  }
+})
+
+//update user phone
+router.put("/:userId/phone", async (req: Request, res: Response) => {
+  try {
+    let user = await User.findOne({ _id: req.params.userId })
+    user.phone = req.body.phone;
+
+    await user.save();
+
+    res.send(
+      `Updated user "${req.params.userId}" with new phone number: "${req.body.phone}"`
+    );
+    
+  } catch (error) {
+    let errorMessage;
+    if (error instanceof Error) { 
+      errorMessage = error.message; 
+    } else { 
+      errorMessage = String(errorMessage); 
+    }
+    res.status(400).send(errorMessage);
+    console.log(`Error: ${errorMessage}`);
   }
 })
 
