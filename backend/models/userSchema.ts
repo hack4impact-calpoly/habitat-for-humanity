046e97906a428d1bd491fd31c71d229914843e93
Brandon Huynh <brandon.huynh@live.com>
2022-03-02 17:16:20 -0800

    Removed irrelevant fields

--- backend/models/userSchema.ts
+++ backend/models/userSchema.ts
@@ -4,23 +4,19 @@ const mongoose = require("mongoose");
 
 interface user {
     userType: string;
-    userID: string;
     firstName: string;
     lastName: string;
     email: string;
     phone: string;
-    picture: ObjectId;
 }
   
 const userSchema = new Schema<user>(
   {
     userType: { type: String, required: true },
-    userID: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, requried: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
-    picture: { type: mongoose.Schema.Types.ObjectId, required: true }
   },
   { collection: "Users" }
 );
