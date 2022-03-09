dea717af7de879d20d5273d3b7bdb94888b50e11
Sebastien Callait <71291167+scallait@users.noreply.github.com>
2022-03-08 23:23:17 -0800

    Added routing for success page

--- frontend/src/components/authentication/SuccessPage/SuccessPage.tsx
+++ frontend/src/components/authentication/SuccessPage/SuccessPage.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import checkCircle from '../../../images/CheckCircle.png';
 require("./SuccessPage.css")
 
 const SuccessPage = (): JSX.Element => {
+    let navigate = useNavigate();
+    const mainScreenPath : string = "/";
+    
+    const buttonNavigation = (e : React.MouseEvent<HTMLButtonElement>) : void => {
+        navigate(mainScreenPath);
+    }
+
     return (
         <>
             <div id="successBox">
                 <img id="checkCircle" src={checkCircle} alt="checkCircle" />
                 <h1>Success</h1>
                 <p>You have successfully signed up for an account.</p>
-                <button id="successButton">Log In</button>
+                <button id="successButton" onClick={buttonNavigation}>Log In</button>
             </div>
         </>
     )
