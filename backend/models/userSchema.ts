03c1ee82c3ef3fd94f115d1bab7c551ef914ece9
Brandon Huynh <brandon.huynh@live.com>
2022-04-17 22:29:27 -0700

    Fixed typo

--- backend/models/userSchema.ts
+++ backend/models/userSchema.ts
@@ -14,7 +14,7 @@ const userSchema = new Schema<user>(
   {
     userType: { type: String, required: true },
     firstName: { type: String, required: true },
-    lastName: { type: String, requried: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
   },
