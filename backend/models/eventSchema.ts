a12a773e1281c1a4bc7bcd7eff45e9d8c601e8e2
Brandon Huynh <brandon.huynh@live.com>
2022-04-27 13:57:53 -0700

    Updated to camel casing

--- backend/models/eventSchema.ts
+++ backend/models/eventSchema.ts
@@ -4,14 +4,14 @@ import { eventConnection } from "../connection";
 
 interface event {
     title: string;
-    pick_up_availability: Types.Array<Types.Array<String>>
+    pickupAvailability: Types.Array<Types.Array<String>>
     location: string;
 }
   
 const eventSchema = new Schema<event>(
   { 
     title: { type: String, required: true },
-    pick_up_availability: { type: [[String]], required: true },
+    pickupAvailability: { type: [[String]], required: true },
     location: { type: String, required: true }
   },
   { collection: "Events" }
