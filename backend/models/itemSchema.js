bc3c4c7833b936eb17c8befcbf0b104d3e46ac4a
Brandon Huynh <brandon.huynh@live.com>
2022-05-09 21:24:18 -0700

    Updated item schema and routes

--- backend/models/itemSchema.js
+++ backend/models/itemSchema.js
@@ -1,6 +1,8 @@
-import { ObjectId, Schema, Types } from 'mongoose';
+// import { ObjectId, Schema, Types } from 'mongoose';
+// const mongoose = require("mongoose");
+const { itemConnection } = require("../connection");
 const mongoose = require("mongoose");
-import { itemConnection } from "../connection";
+
 
 // interface item {
 //     name: string;
@@ -17,21 +19,24 @@ import { itemConnection } from "../connection";
 //     time_accepted: string;
 // }
 
-const itemSchema = new Schema(
+const itemSchema = new mongoose.Schema(
   {
-    name: {type: String, required: false },
-    images: { type: [mongoose.Schema.Types.ObjectId], required: false },
+    name: {type: String, required: true },
+    images: { type: [mongoose.Schema.ObjectId], required: true },
     size: { type: String, required: true },
-    address: {type: String, required: false },
-    city: { type: String, required: false },
+    address: {type: String, required: true },
+    city: { type: String, required: true },
     zipCode: { type: String, required: true },
-    donor_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    scheduling: {type: String, required: true},
+    timeAvailability: { type: [[Date, Date]], required: true },
+    donorId: { type: mongoose.Schema.Types.ObjectId, required: true },
+    timeSubmitted: { type: Date, required: true },
+    timeApproved: { type: Date, required: false },
+    status: { type: String, required: true },
     notes: { type: String, required: false },
-    time_submitted: { type: String, required: false },
-    time_accepted: { type: String, required: false }
   },
   { collection: "Items" }
 );
   
 const Item = itemConnection.model("Items", itemSchema);
-export default Item;
+module.exports = Item;
