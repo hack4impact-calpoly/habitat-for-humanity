195ba63383a0729dd01f51d92bab7d1c11e0a1a3
Brandon Huynh <brandon.huynh@live.com>
2022-05-09 21:23:22 -0700

    Updated Event schema and routes

--- backend/models/eventSchema.js
+++ backend/models/eventSchema.js
@@ -1,6 +1,9 @@
-import { Schema, Types } from 'mongoose';
+// import { Schema, Types } from 'mongoose';
+// const mongoose = require("mongoose");
+// import { eventConnection } from "../connection";
+
+const { eventConnection } = require("../connection");
 const mongoose = require("mongoose");
-import { eventConnection } from "../connection";
 
 // interface event {
 //     title: string;
@@ -8,14 +11,25 @@ import { eventConnection } from "../connection";
 //     location: string;
 // }
   
-const eventSchema = new Schema(
+const eventSchema = new mongoose.Schema(
   { 
     title: { type: String, required: true },
-    pick_up_availability: { type: [[String]], required: true },
-    location: { type: String, required: true }
+    startTime: { type: Date, required: true },
+    endTime: { type: Date, required: true },
+    volunteerId: { type: mongoose.Types.ObjectId, required: true },
+    itemId: { type: mongoose.Types.ObjectId, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    volunteerFirstName: { type: String, required: true },
+    volunteerLastName: { type: String, required: true },
+    donorFirstName: { type: String, required: true },
+    donorLastName: { type: String, required: true },
+    itemName: { type: String, required: true },
+    phone: { type: String, required: true },
   },
   { collection: "Events" }
 );
   
 const Event = eventConnection.model("Events", eventSchema);
-export default Event;
+module.exports = Event;
