d7a6853b910342f036234c92400287ab3acb7b88
jin <jinwu2003@gmail.com>
2022-05-11 19:08:20 -0700

    Connect Amplify Auth with Our Screens #81

--- frontend/src/components/authentication/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ frontend/src/components/authentication/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Auth } from "aws-amplify";
 require("./ForgotPasswordPage.css");
 
 const ForgotPasswordPage = (): JSX.Element => {
     const [email, setEmail] = useState<string>("");
-    
+
     let navigate = useNavigate();
 
-    const buttonNavigation = (e : React.MouseEvent<HTMLButtonElement>) : void => {
-        const mainScreenPath : string = "/"; // Main screen (login)
-        
-        if(e.currentTarget.value === "sendButton"){
-            if(submitData()){
-                navigate(mainScreenPath);
+    // Function for calling Auth.forgotPassword, called in buttonNavigation
+    let awsForgotPassword = async (): Promise<any> => {
+        let response = await Auth.forgotPassword(email).catch(
+            error => {
+                const code = error.code;
+                console.log(error);
+                switch (code) {
+                    case 'UserNotFoundException':
+                        alert("Please enter a valid email");
+                        return false;
+                    case 'LimitExceededException':
+                        alert("Too many tries, pleast wait");
+                        return false;
+                }
+            }
+        );
+        return true;
+    }
+    const buttonNavigation = async (e: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
+        const mainScreenPath: string = "/"; // Main screen (login)
+        const successPath: string = "/NewPasswordPage"
+        let checkAWS = await awsForgotPassword();
+        if (e.currentTarget.value === "sendButton") {
+            if (submitData() && checkAWS) {
+                navigate(successPath);
             }
         }
     }
 
-    const submitData = () : boolean => {
-        if (validateEmail())
-        {
+    const submitData = (): boolean => {
+        if (validateEmail()) {
             const JSONstring = getFormData();
             console.log(JSONstring);
             //connect to backend code
@@ -28,7 +47,7 @@ const ForgotPasswordPage = (): JSX.Element => {
         return false;
     }
 
-    const getFormData = () : string => {
+    const getFormData = (): string => {
         const forgotEmail = {
             "email": email
         };
@@ -40,36 +59,34 @@ const ForgotPasswordPage = (): JSX.Element => {
         Desc: Validates email
         Return: boolean (true if valid, false if not)
         */
-        if (email === "")
-        {
+        if (email === "") {
             alert("Please add email");
             return false;
         }
-        else if (!email.includes("@"))
-        {
+        else if (!email.includes("@")) {
             alert("Please enter a valid email address");
             return false;
         }
         //else if (check if email does NOT exist)
-            //alert("There is no account associated with this email") 
-        return true;    
+        //alert("There is no account associated with this email") 
+        return true;
     }
-    
+
     //HTML Body
-    return(
-    <body>
-        <div id="forgotPasswordBox">
-            <p id="forgotPasswordText">Forgot Password</p>
-            <p className="forgotPasswordMessage">Please enter the email associated with your account to receive a confirmation code.</p>
+    return (
+        <body>
+            <div id="forgotPasswordBox">
+                <p id="forgotPasswordText">Forgot Password</p>
+                <p className="forgotPasswordMessage">Please enter the email associated with your account to receive a confirmation code.</p>
                 <p className="emailInput">Email</p>
                 <input className="inputBox"
                     type="text"
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e?.target?.value)}
                 />
-            <button value="sendButton" id="sendButton" onClick={buttonNavigation}>Send</button>
+                <button value="sendButton" id="sendButton" onClick={buttonNavigation}>Send</button>
 
-        </div>
-    </body>);
+            </div>
+        </body>);
 }
 
 export default ForgotPasswordPage;
