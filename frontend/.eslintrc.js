2a47db65bd8e0d00f4c4db929b597b9907108fb6
Josh Wong <joshwong74@gmail.com>
2022-10-28 22:06:47 -0700

    Added linting according to docs

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+  ],
+  overrides: [
+    {
+      files: ["**/*.test.js", "**/*.test.jsx", "**/*.test.ts", "**/*.test.tsx"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    project: "./tsconfig.eslint.json",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    // allow prop-spreading
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": "off",
+    "no-underscore-dangle": "off",
+    "no-plusplus": "off",
+    "no-console": "off",
+  },
+};
