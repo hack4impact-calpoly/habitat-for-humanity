6639aba003e63a8b2fc6523ba5d5fbd8a149cd7e
scallait <sebastiencallait@gmail.com>
2022-11-17 01:12:43 -0800

    Set up linting backend

---
+++ backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 0,
+    'no-underscore-dangle': 0,
+    'no-plusplus': 'off',
+  },
+};
