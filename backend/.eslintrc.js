02821b93bea2534e8b32a09b9830f6fd18c613a7
scallait <sebastiencallait@gmail.com>
2022-11-17 01:13:03 -0800

    Update .eslintrc.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -4,15 +4,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: "latest",
   },
-  plugins: ['prettier'],
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    'no-console': 0,
-    'no-underscore-dangle': 0,
-    'no-plusplus': 'off',
+    "prettier/prettier": "error",
+    "no-console": 0,
+    "no-underscore-dangle": 0,
+    "no-plusplus": "off",
   },
 };
