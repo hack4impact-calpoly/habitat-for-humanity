45d380ad0bbbfcebdf34acb67ebb6a1f1247fa4d
Josh Wong <joshwong74@gmail.com>
2023-02-04 21:27:10 -0600

    Donor profile

--- frontend/src/components/donor/DonorProfile/DonorProfile.tsx
+++ frontend/src/components/donor/DonorProfile/DonorProfile.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
 import pencil from "images/pencil.png";
 import { Auth } from "aws-amplify";
 import { getUserByID } from "api/user";
@@ -26,51 +27,59 @@ function DonatorProfilePage(): JSX.Element {
   return (
     <div>
       <DonatorNavbar />
-      <div id="donatorProfileBox">
-        <div id="headerBox">
-          <div id="profileBox">
-            <p id="profile">Profile</p>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div id="donatorProfileBox">
+          <div id="headerBox">
+            <div id="profileBox">
+              <p id="profile">Profile</p>
+            </div>
+            <div id="editBox">
+              <img alt="pencil" id="pencil" src={pencil} />
+              {/* Need to Implement Link to DonatorProfileEditPage */}
+              <Link to={donatorProfileEditPath} id="edit">
+                edit
+              </Link>
+            </div>
           </div>
-          <div id="editBox">
-            <img alt="pencil" id="pencil" src={pencil} />
-            {/* Need to Implement Link to DonatorProfileEditPage */}
-            <Link to={donatorProfileEditPath} id="edit">
-              edit
-            </Link>
+          <div id="nameBox">
+            <div className="headerBox">
+              <p className="infoHeader">Name</p>
+            </div>
+            <div className="infoBox">
+              {/* Need to implement displaying user data from backend */}
+              <p id="name">
+                {user?.firstName && user?.lastName
+                  ? `${user.firstName} ${user.lastName}`
+                  : ""}
+              </p>
+            </div>
+          </div>
+          <div id="emailBox">
+            <div className="headerBox">
+              <p className="infoHeader">Email</p>
+            </div>
+            <div className="infoBox">
+              {/* Need to implement displaying user data from backend */}
+              <p id="email">{user?.email}</p>
+            </div>
+          </div>
+          <div id="phoneBox">
+            <div className="headerBox">
+              <p className="infoHeader">Phone</p>
+            </div>
+            <div className="infoBox">
+              {/* Need to implement displaying user data from backend */}
+              <p id="phone">{user?.phone}</p>
+            </div>
           </div>
         </div>
-        <div id="nameBox">
-          <div className="headerBox">
-            <p className="infoHeader">Name</p>
-          </div>
-          <div className="infoBox">
-            {/* Need to implement displaying user data from backend */}
-            <p id="name">
-              {user?.firstName && user?.lastName
-                ? `${user.firstName} ${user.lastName}`
-                : ""}
-            </p>
-          </div>
-        </div>
-        <div id="emailBox">
-          <div className="headerBox">
-            <p className="infoHeader">Email</p>
-          </div>
-          <div className="infoBox">
-            {/* Need to implement displaying user data from backend */}
-            <p id="email">{user?.email}</p>
-          </div>
-        </div>
-        <div id="phoneBox">
-          <div className="headerBox">
-            <p className="infoHeader">Phone</p>
-          </div>
-          <div className="infoBox">
-            {/* Need to implement displaying user data from backend */}
-            <p id="phone">{user?.phone}</p>
-          </div>
-        </div>
-      </div>
+      </Box>
     </div>
   );
 }
