b2d84c6c68abd08d26abb955b89a79b18440f865
jwu205 <jinwu2003@gmail.com>
2023-02-26 18:54:51 -0800

    Fixed frontend view bug for Time Availability Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,7 @@ import LoginPage from "./components/authentication/LoginPage/LoginPage";
 // import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
 // import DonatorNextStepsPage from './components/donator/DonatorNextStepsPage/DonatorNextStepsPage';
 // import Donation from './components/donator/Donation/Donation';
-// import DonorSchedulePickUp from './components/donor/DonorScheduleDropoffPickupPage/DonorSchedulePickUp';
+import DonorSchedulePickUp from "./components/donor/DonorScheduleDropoffPickupPage/DonorSchedulePickUp";
 
 import awsExports from "./aws-exports";
 // import VerifyAccountPage from 'components/authentication/VerifyAccountPage/VerifyAccountPage';
@@ -46,10 +46,10 @@ function App(): JSX.Element {
     // <LoginPage />
     // <DonatorHomePage />
     // <CreateAccountPage />
-    // <DonorSchedulePickUp/>
+    <DonorSchedulePickUp/>
     // <DonatorProfileEditPage />
     // <DonatorLocationPage />
-    <LoginPage />
+    // <LoginPage />
     // <DonatorNextStepsPage />
     // <VerifyAccountPage />
     // <SubmitDropOffPage />
