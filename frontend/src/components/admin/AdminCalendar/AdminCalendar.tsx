891c20c231d6bd566a3dbc1b73ee2cd3da6417eb
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2023-02-28 15:56:44 -0800

    Added prototype calendar

--- frontend/src/components/admin/AdminCalendar/AdminCalendar.tsx
+++ frontend/src/components/admin/AdminCalendar/AdminCalendar.tsx
@@ -1,6 +1,19 @@
+import FullCalendar from "@fullcalendar/react";
 import React from "react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import AdminNavbar from "../AdminNavbar/AdminNavbar";
+
+require("./AdminCalendar.css");
 
 function AdminCalendar(): JSX.Element {
-  return <div>hello</div>;
+  return (
+    <div>
+      <AdminNavbar />
+
+      <div>
+        <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      </div>
+    </div>
+  );
 }
 export default AdminCalendar;
