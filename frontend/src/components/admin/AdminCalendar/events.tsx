6a770672606087a6a3a7bda94fa91fb97c12ef55
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2023-03-08 19:41:45 -0800

    mostly complete view for admin calendar

---
+++ frontend/src/components/admin/AdminCalendar/events.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+
+//gonna make this the data farm 
+
+
+//event
+{[
+    {
+      textColor: "black",
+      backgroundColor: "transparent",
+      borderColor: "transparent",
+      title: `Jane Lee 1 Mustang Drive
+      San Luis Obispo, Ca
+      93407
+      `,
+      start: "2023-03-07T09:00:00",
+      end: "2023-03-07T11:00:00",
+    },
+    {
+      textColor: "black",
+      backgroundColor: "transparent",
+      borderColor: "transparent",
+      title: `Jane Lee 1 Mustang Drive
+      San Luis Obispo, Ca
+      93407
+      `,
+      start: "2023-02-26T08:00:00",
+      end: "2023-02-26T11:00:00",
+    },
+  ]}
