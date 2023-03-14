542e13c8836aee6fa7043d0898a8c4aef5e18f66
sam-hud <sam.hud@icloud.com>
2023-03-14 10:28:35 -0700

    fix: move admin schedule page to component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -33,7 +33,8 @@ import LoginPage from "./components/authentication/LoginPage/LoginPage";
 // import DonatorLocationPage from './components/donator/DonationLocationPage/DonatorLocationPage';
 // import DonatorNextStepsPage from './components/donator/DonatorNextStepsPage/DonatorNextStepsPage';
 // import Donation from './components/donator/Donation/Donation';
-// import DonorSchedulePickUp from "./components/donor/DonorScheduleDropoffPickupPage/DonorSchedulePickUp";
+// import DonorSchedulePickUp from './components/donor/DonorScheduleDropoffPickupPage/DonorSchedulePickUp';
+import ActiveDonationPage from "./components/admin/ActiveDonationsPage/ActiveDonationsPage";
 
 import awsExports from "./aws-exports";
 // import VerifyAccountPage from 'components/authentication/VerifyAccountPage/VerifyAccountPage';
