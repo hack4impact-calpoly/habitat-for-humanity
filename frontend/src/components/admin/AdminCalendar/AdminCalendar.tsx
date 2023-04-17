74d3d8969c94f2e1e0f35d8de0ff5bce4ef1387a
eduardo661 <73264386+eduardo661@users.noreply.github.com>
2023-04-17 15:41:57 -0700

    added popover component

--- frontend/src/components/admin/AdminCalendar/AdminCalendar.tsx
+++ frontend/src/components/admin/AdminCalendar/AdminCalendar.tsx
@@ -1,55 +1,91 @@
-import FullCalendar, { EventContentArg } from "@fullcalendar/react";
-import React from "react";
+import FullCalendar, {
+  EventClickArg,
+  EventContentArg,
+} from "@fullcalendar/react";
+import React, { useState } from "react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import { Button, Modal, Box, Typography } from "@mui/material";
 import AdminNavbar from "../AdminNavbar/AdminNavbar";
 
 require("./AdminCalendar.css");
 
-// custom layout for a given event cell
-function customEvent(args: EventContentArg) {
-  return (
-    <div>
-      <h1
-        style={{
-          display: "flex",
-          fontSize: "15px",
-          justifyContent: "center",
-          marginTop: "0px",
-          marginBottom: "0px",
-        }}
-      >
-        {args.event.extendedProps.name}
-      </h1>
-
-      <div
-        style={{ display: "flex", justifyContent: "center", fontSize: "10px" }}
-      >
-        {args.event.extendedProps.address}
-      </div>
-    </div>
-  );
-}
-
-function addCustomClassNames(info: any) {
-  // add classnames that decide how each cell is styled
-  // used to add past days grey color
-  const classNames = [];
-
-  if (info.date < new Date()) {
-    classNames.push("past-day");
-  }
-
-  if (info.isToday) {
-    classNames.push("today");
-  }
-
-  return classNames;
-}
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  height: 350,
+  bgcolor: "background.paper",
+  border: "2px solid grey",
+  boxShadow: 24,
+  p: 4,
+};
 
 function AdminCalendar(): JSX.Element {
+  const [open, setOpen] = useState(false);
+  const [clickedEvent, setEvent] = useState<EventClickArg>();
+
+  const renderModalComponent = (args: EventClickArg) => {
+    console.log(args.event.extendedProps.address);
+    setEvent(args);
+    setOpen(true);
+  };
+
+  const closeModalComponent = () => {
+    setOpen(false);
+  };
+
   return (
-    <div style={{ height: "100vh" }}>
+    <div>
+      {open ? (
+        <Modal
+          open={open}
+          onClose={closeModalComponent}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {clickedEvent!.event.extendedProps.address}
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              <p>
+                {new Date(clickedEvent!.event.start!).toLocaleDateString(
+                  "en-US",
+                  {
+                    weekday: "long",
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  }
+                )}
+                -
+                {new Date(clickedEvent!.event.end!).toLocaleTimeString(
+                  "en-US",
+                  {
+                    hour: "numeric",
+                    minute: "numeric",
+                    hour12: true,
+                  }
+                )}
+                <p> need to fetch name of person assigned to this</p>
+              </p>
+              <p>donor : {clickedEvent!.event.extendedProps.name}</p>
+              <p>item: (need to fetch this)</p>
+              <p>phone : {clickedEvent!.event.extendedProps.phone}</p>
+              <button type="button">view donnation</button>
+            </Typography>
+          </Box>
+        </Modal>
+      ) : (
+        console.log("no valid data")
+      )}
+
       <AdminNavbar />
 
       <div id="smallCalendar" style={{ display: "flex", height: "100%" }}>
@@ -92,6 +128,7 @@ function AdminCalendar(): JSX.Element {
             slotMaxTime="18:00:00"
             slotDuration="01:00:00"
             eventContent={customEvent}
+            eventClick={renderModalComponent}
             dayCellClassNames={addCustomClassNames}
             titleFormat={{
               month: "long",
@@ -147,5 +184,45 @@ function AdminCalendar(): JSX.Element {
       </div>
     </div>
   );
+}
+
+function customEvent(args: EventContentArg) {
+  return (
+    <div>
+      <h1
+        style={{
+          display: "flex",
+          fontSize: "15px",
+          justifyContent: "center",
+          marginTop: "0px",
+          marginBottom: "0px",
+        }}
+      >
+        {args.event.extendedProps.name}
+      </h1>
+
+      <div
+        style={{ display: "flex", justifyContent: "center", fontSize: "10px" }}
+      >
+        {args.event.extendedProps.address}
+      </div>
+    </div>
+  );
+}
+
+function addCustomClassNames(info: any) {
+  // add classnames that decide how each cell is styled
+  // used to add past days grey color
+  const classNames = [];
+
+  if (info.date < new Date()) {
+    classNames.push("past-day");
+  }
+
+  if (info.isToday) {
+    classNames.push("today");
+  }
+
+  return classNames;
 }
 export default AdminCalendar;
---
+++ frontend/src/components/admin/AdminCalendar/modalComponent.tsx
@@ -0,0 +1,6 @@
+import { Button, Modal, Box, Typography } from "@mui/material";
+import React from "react";
+
+function modalComponent(): JSX.Element {
+  return <div>hello</div>;
+}
