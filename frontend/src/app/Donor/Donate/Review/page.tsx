b8202d8a56ec521575f78cdf30e2a4835882758f
Peter Chinh <peterchinh1018@gmail.com>
2025-02-24 17:20:22 -0800

    fix: Fetch user id with clerk

--- frontend/src/app/Donor/Donate/Review/page.tsx
+++ frontend/src/app/Donor/Donate/Review/page.tsx
@@ -9,6 +9,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { Item, addItem } from "../../../../api/item";
 import { addImages, getImages, getImageByID } from "../../../../api/image";
 import { RootState } from "../../../../redux/store";
+import { useAuth } from "@clerk/nextjs";
 
 require("../../../../App.css");
 
@@ -30,6 +31,7 @@ const SubmitInfo: React.FC<DummyComponentProps> = ({
   dropOff,
   component,
 }) => {
+  const { userId } = useAuth();
   const storedDonation = useSelector((state: RootState) => state.donation);
 
   const storedName = useSelector((state: RootState) => state.donation.name);
@@ -59,16 +61,9 @@ const SubmitInfo: React.FC<DummyComponentProps> = ({
   );
   const dispatch = useDispatch();
 
-  const setCurrentUserID = async () => {
-    Auth.currentUserInfo().then((user) => {
-      const { attributes = {} } = user;
-      dispatch(updateDonorID(attributes["custom:id"]));
-    });
-  };
-
   useEffect(() => {
-    setCurrentUserID();
-  }, []);
+    dispatch(updateDonorID(userId));
+  }, [userId]);
 
   name = storedName;
   dimensions = storedDimensions;
