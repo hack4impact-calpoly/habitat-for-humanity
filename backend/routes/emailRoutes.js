94dffb62f255d987de146600b3a21454274af3cf
ljbarker <loganjb21@outlook.com>
2025-02-24 18:55:05 -0800

    fix: update email env names

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -1,74 +1,75 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const multer = require("multer");
-const router = express.Router()
+const router = express.Router();
 
-require("dotenv/config")
+require("dotenv/config");
 
 // POST /api/email/
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const { recipientEmail, subject, body, isHTML } = req.body;
 
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: "Gmail",
       auth: {
-        user: process.env.email_address,
-        pass: process.env.email_password,
+        user: process.env.H4H_EMAIL,
+        pass: process.env.H4H_EMAIL_PASSWORD,
       },
     });
 
-    let mailOptions = ''
+    let mailOptions = "";
     // Prepare the email message
     if (isHTML) {
       mailOptions = {
-        from: process.env.email_address,
+        from: process.env.H4H_EMAIL,
         to: recipientEmail,
-        subject: subject ,
+        subject: subject,
         html: body, // Use the provided body as HTML
       };
     } else {
       mailOptions = {
-        from: process.env.email_address,
+        from: process.env.H4H_EMAIL,
         to: recipientEmail,
-        subject: subject ,
+        subject: subject,
         text: body, // Use the provided body as plain text
       };
     }
 
     // Send the email
     await transporter.sendMail(mailOptions);
-    
-    res.status(200).json({ message: 'Custom email sent successfully' }); // Return a success response
+
+    res.status(200).json({ message: "Custom email sent successfully" }); // Return a success response
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'An error occurred while sending the custom email' }); // Return an error response
+    console.log(error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while sending the custom email" }); // Return an error response
   }
 });
-
 
 // POST /api/email/attach-files
 const storage = multer.memoryStorage(); // Create a Multer storage configuration
 const upload = multer({ storage });
 
-router.post('/attach-files', upload.array('attachments'), async (req, res) => {
+router.post("/attach-files", upload.array("attachments"), async (req, res) => {
   try {
     const { recipientEmail, subject, body, isHTML } = req.body;
     const attachments = req.files;
 
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: "Gmail",
       auth: {
-        user: process.env.email_address,
-        pass: process.env.email_password,
+        user: process.env.H4H_EMAIL,
+        pass: process.env.H4H_EMAIL_PASSWORD,
       },
     });
 
-    let mailOptions = '';
+    let mailOptions = "";
     // Prepare the email message
     if (isHTML) {
       mailOptions = {
-        from: process.env.email_address,
+        from: process.env.H4H_EMAIL,
         to: recipientEmail,
         subject: subject,
         html: body, // Use the provided body as HTML
@@ -80,7 +81,7 @@ router.post('/attach-files', upload.array('attachments'), async (req, res) => {
       };
     } else {
       mailOptions = {
-        from: process.env.email_address,
+        from: process.env.H4H_EMAIL,
         to: recipientEmail,
         subject: subject,
         text: body, // Use the provided body as plain text
@@ -95,32 +96,34 @@ router.post('/attach-files', upload.array('attachments'), async (req, res) => {
     // Send the email
     await transporter.sendMail(mailOptions);
 
-    res.status(200).json({ message: 'Custom email sent successfully' }); // Return a success response
+    res.status(200).json({ message: "Custom email sent successfully" }); // Return a success response
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'An error occurred while sending the custom email' }); // Return an error response
+    res
+      .status(500)
+      .json({ error: "An error occurred while sending the custom email" }); // Return an error response
   }
 });
 
 // POST /api/email/donation-approved-scheduled
-router.post('/donation-approved-scheduled', async (req, res) => {
+router.post("/donation-approved-scheduled", async (req, res) => {
   try {
     const { recipientEmail, donationDetails } = req.body;
 
     // Create a Nodemailer transporter
     const transporter = nodemailer.createTransport({
-      service: 'Gmail',
+      service: "Gmail",
       auth: {
-        user: process.env.email_address,
-        pass: process.env.email_password,
+        user: process.env.H4H_EMAIL,
+        pass: process.env.H4H_EMAIL_PASSWORD,
       },
     });
 
     // Prepare the email message
     const mailOptions = {
-      from: process.env.email_address,
+      from: process.env.H4H_EMAIL,
       to: recipientEmail,
-      subject: 'Donation Approved & Scheduled',
+      subject: "Donation Approved & Scheduled",
       html: `<p>${donationDetails.name},</p>
              <p>Thank you for your donation!</p>
              <p>Your donation has been approved and scheduled for pickup or drop-off. Here are the details:</p>
@@ -136,12 +139,17 @@ router.post('/donation-approved-scheduled', async (req, res) => {
     await transporter.sendMail(mailOptions);
 
     // Return a success response
-    res.status(200).json({ message: 'Donation approval and schedule email sent successfully' });
+    res.status(200).json({
+      message: "Donation approval and schedule email sent successfully",
+    });
   } catch (error) {
-    console.error('Error sending donation approval and schedule email:', error);
+    console.error("Error sending donation approval and schedule email:", error);
     // Return an error response
-    res.status(500).json({ error: 'An error occurred while sending the donation approval and schedule email' });
+    res.status(500).json({
+      error:
+        "An error occurred while sending the donation approval and schedule email",
+    });
   }
 });
 
-module.exports = router
+module.exports = router;
