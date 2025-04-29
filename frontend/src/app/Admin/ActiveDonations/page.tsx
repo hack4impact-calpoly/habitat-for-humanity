2ed6c47c1ded9fd7c402ef05b083e3356f5cd0a0
Kailuan Liu <kailuan1127@gmail.com>
2025-04-29 10:34:01 -0700

    donationTable, admin history page

--- frontend/src/app/Admin/ActiveDonations/page.tsx
+++ frontend/src/app/Admin/ActiveDonations/page.tsx
@@ -1,5 +1,12 @@
-import DonationTable from "components/admin/DonationTable/page";
+// app/admin/active/page.tsx
+import AdminNavbar from "components/admin/AdminNavbar/AdminNavbar";
+import DonationsTable from "components/DonationTable/page";
 
 export default function ActiveDonationsPage() {
-  return <DonationTable mode="active" />;
+  return (
+    <div>
+      <AdminNavbar />
+      <DonationsTable viewType="active" />
+    </div>
+  );
 }
