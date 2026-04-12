0c435cab43e92fbf08ee75e35721f82767101250
parnika-singh11 <parnika110307@gmail.com>
2026-04-12 12:37:49 -0700

    Add DonationDetails admin table for completed donations

--- src/app/Admin/History/page.tsx
+++ src/app/Admin/History/page.tsx
@@ -1,12 +1,12 @@
 // app/admin/history/page.tsx
 import AdminNavbar from "components/admin/AdminNavbar/AdminNavbar";
-import DonationsTable from "components/DonationTable/page";
+import DonationDetails from "components/admin/DonationDetails/DonationDetails";
 
 export default function DonationHistoryPage() {
   return (
     <div>
       <AdminNavbar />
-      <DonationsTable viewType="history" />
+      <DonationDetails />
     </div>
   );
 }
